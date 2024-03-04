2e1d62f071afdcb12bf4f02e47c9b0323f7d03b5
Samuel Escalera <samuel.escalera.9413603@usalesiana.edu.bo>
2024-03-04 02:02:52 -0400

    Docs

--- src/main/java/org/example/PlagiarismChecker.java
+++ src/main/java/org/example/PlagiarismChecker.java
@@ -6,7 +6,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta clase representa una solución para identificar la similitud entre dos textos y encontrar palabras mal escritas.
+ * Utiliza el algoritmo de Levenshtein para calcular la distancia de edición entre las cadenas de texto.
+ */
+
 public class PlagiarismChecker {
+
+    /**
+     * Calcula la distancia de Levenshtein entre dos cadenas de texto.
+     *
+     * @param s1 La primera cadena de texto.
+     * @param s2 La segunda cadena de texto.
+     * @return La distancia de Levenshtein entre las dos cadenas.
+     */
 
     public static int levenshteinDistance(String s1, String s2) {
         int m = s1.length();
@@ -33,11 +46,25 @@ public class PlagiarismChecker {
     }
 
 
+    /**
+     * Calcula el porcentaje de similitud entre dos textos basado en la distancia de Levenshtein.
+     *
+     * @param text1 El primer texto.
+     * @param text2 El segundo texto.
+     * @return El porcentaje de similitud entre los textos.
+     */
+
     public static double calculateSimilarity(String text1, String text2) {
         int distance = levenshteinDistance(text1, text2);
         return 100.0 - ((double) distance / (double) text1.length()) * 100.0;
     }
 
+    /**
+     * Encuentra y muestra las palabras mal escritas en el segundo texto.
+     *
+     * @param text1 El primer texto.
+     * @param text2 El segundo texto.
+     */
 
     public static void findMisspelledWords(String text1, String text2) {
         String[] words1 = text1.split("\\s+");
@@ -85,6 +112,13 @@ public class PlagiarismChecker {
 
         findMisspelledWords(text1, text2);
     }
+
+    /**
+     * Lee el contenido de un archivo de texto y lo devuelve como una cadena.
+     *
+     * @param filename El nombre del archivo a leer.
+     * @return El contenido del archivo como una cadena.
+     */
 
     public static String readFile(String filename) {
         StringBuilder content = new StringBuilder();
